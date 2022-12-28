3a1f2bac19e6fb13acccbdfd2421351015abc5e8
pavithra147 <pavithra.jayavel@aspiresys.com>
2022-12-28 17:14:25 +0530

    changes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -10,7 +10,7 @@ export class AdminComponent implements OnInit {
  Admin!: { id: string; name: string; };  //Assume admin variable with id and name to get the data
   constructor(private router: ActivatedRoute) { }  //"ACTIVATEDROUTE " gives information about current Route
 
-  ngOnInit(): void {         // one drawback of using snapshot 
+  ngOnInit(): void {                     // one drawback of using snapshot 
     this.Admin = {                //ngOnInit() it will load the component once it will not reload the component
       id: this.router.snapshot.params['id'],   //To get the data from url from template of component inside the [] is identifier
       name: this.router.snapshot.params['name'],  
